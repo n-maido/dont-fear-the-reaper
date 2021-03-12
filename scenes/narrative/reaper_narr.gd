25e859e6d772d80cb0a2ecfb28ff694ba59c4341
n-maido <nhi_maido@outlook.com>
2021-03-12 12:15:20 -0800

    add keybindings and resize photos

--- scenes/narrative/reaper_narr.gd
+++ scenes/narrative/reaper_narr.gd
@@ -4,7 +4,7 @@ var dialog = preload("res://scenes/narrative/dialog.tscn")
 
 const images = [
 	preload("res://scenes/narrative/assets/hell.jpg"),
-	preload("res://scenes/narrative/assets/parchment.jpg")
+	preload("res://scenes/narrative/assets/parchment.png")
 ]
 
 var reaper_dialog
@@ -12,10 +12,11 @@ var reaper_dialog
 func _ready():
 	reaper_dialog = dialog.instance()
 	reaper_dialog.phrases = [
+		'Press the arrow buttons, or use the arrow keys on your keyboard to navigate. Or, press SPACE to skip the introduction',
 		'Ahh, another day, another death. Time get started with the workday. Let me consult my list...',
 		'I have my little list of what I call “the TBDs” (To Be Deceased). My job is to visit each person and rip their souls out of their body. I’m no different from Santa Claus, really.',
 		'It’s honest work. I get to travel the world and meet new people, but things are not always easy.',
-		'People keep screaming and running away from me. I’m just trying to escort them! Hopefully one day, people can truly appreciate me.',
+		'People keep screaming and running away from me. I’m just trying to escort them to their deaths! Hopefully one day, people can truly appreciate me for my hard work.',
 		'Anyways, back to the list. It looks like Tom is next...'
 	]
 	add_child(reaper_dialog)
@@ -26,9 +27,7 @@ func _process(delta):
 	load_background()
 
 func load_background():
-	if reaper_dialog.phrase_index == 1 or reaper_dialog.phrase_index == 2 or reaper_dialog.phrase_index == 4:
-		$BackgroundImage.set_texture(images[1])
-	elif reaper_dialog.phrase_index == 3:
+	if reaper_dialog.phrase_index >= 2  and reaper_dialog.phrase_index <= 5:
 		$BackgroundImage.set_texture(images[1])
 	else:
 		$BackgroundImage.set_texture(images[0])
--- scenes/narrative/tbd_narr.gd
+++ scenes/narrative/tbd_narr.gd
@@ -3,15 +3,19 @@ extends Node2D
 var dialog = preload("res://scenes/narrative/dialog.tscn")
 
 const images = [
-	preload("res://scenes/narrative/assets/hell.jpg"),
-	preload("res://scenes/narrative/assets/parchment.jpg")
+	preload("res://scenes/narrative/assets/village.png"),
+	preload("res://scenes/narrative/assets/alley.png"),
+	preload("res://scenes/narrative/assets/reaper.png"),
+	preload("res://scenes/narrative/assets/catacomb.png")
 ]
 
 var tbd_dialog
 
 func _ready():
+	# OS.window_fullscreen = true
 	tbd_dialog = dialog.instance()
 	tbd_dialog.phrases = [
+		'Press the arrow buttons, or use the arrow keys on your keyboard to navigate. Or, press SPACE to skip the introduction',
 		'Ahh, another day, another dawn. Time to walk to my job at the market.',
 		'It’s honest work. I get to work outside and meet new people, but things are not always easy.',
 		'Sometimes, thieves run off with goods. Plus, my boss can be a bit demanding, and he makes me chase after them.',
@@ -27,14 +31,14 @@ func _process(delta):
 	load_background()
 
 func load_background():
-	if tbd_dialog.phrase_index == 2:
-		$BackgroundImage.set_texture(images[1])
-	elif tbd_dialog.phrase_index == 3:
-		$BackgroundImage.set_texture(images[1])
+	if tbd_dialog.phrase_index == 3:
+		$BackgroundImage.set_texture(images[0])
 	elif tbd_dialog.phrase_index == 4:
 		$BackgroundImage.set_texture(images[1])
 	elif tbd_dialog.phrase_index == 5:
-		$BackgroundImage.set_texture(images[1])
+		$BackgroundImage.set_texture(images[2])
+	elif tbd_dialog.phrase_index == 6:
+		$BackgroundImage.set_texture(images[3])
 	else:
 		$BackgroundImage.set_texture(images[0])
 	
