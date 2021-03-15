04f10648a0e9f97931637664948c8ab2e33a4ec0
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-15 11:52:59 +0530

    Removed Common Assets

--- scenes/title/titlescreen.gd
+++ scenes/title/titlescreen.gd
@@ -9,6 +9,7 @@ var scene_path_to_load
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
