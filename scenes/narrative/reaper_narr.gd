a18fd385ecb3ed304f97128276aae025387cdea1
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-03-17 19:57:25 +0530

    Added play as scene and integrated it with narration

--- scenes/narrative/reaper_narr.gd
+++ scenes/narrative/reaper_narr.gd
@@ -35,4 +35,4 @@ func load_background():
 	
 func _input(event):
 	if Input.is_action_pressed("ui_select"):
-		get_tree().change_scene("res://scenes/MainScene.tscn")
+		get_tree().change_scene("res://scenes/reaper_gameplay/ReaperPlayer.tscn")
